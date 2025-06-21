56cdd1ef7d1243dda2e629c477f40ef05dbaaf8e
teerapong07 <teerapong.nokaew07@gmail.com>
2025-06-21 19:53:48 +0700

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,44 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors"
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
 
-import notesRoutes from "./routes/notes.route.js";
-import { connectDB } from "./config/db.js";
-import rateLimiter from "./middleware/rateLimiter.js";
+import notesRoutes from './routes/notes.route.js';
+import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 connectDB();
 
-app.use(cors({
-  origin:"http://localhost:5173",
-}))
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    })
+  );
+}
 app.use(express.json());
-app.use(rateLimiter)
-
+app.use(rateLimiter);
 
 // app.use((req, res, next) => {
 //   console.log(`Req method is ${req.method} & Req Url is ${req.url}`);
 //   next();
 // });
 
-app.use("/api/notes", notesRoutes);
+app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
